7ca8f0df723787ec627afc2f3d30dff26750b409
devfast01 <98269086+devfast01@users.noreply.github.com>
2022-01-29 21:46:54 +0500

    Add files via upload

---
+++ Javascript crash course/home.js
@@ -0,0 +1,15 @@
+console.log('Hello Javascript')
+console.log('WELLCOME')
+
+// alert("heeeyyy  -_-")
+
+// Variables 
+var b = 'Welliyew'
+console.log(b)
+
+var  someNumber = 10
+console.log(someNumber)
+
+var age = prompt('What is your age ?')
+
+document.getElementById('someText').innerHTML = age
